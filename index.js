a8acc483c596746ba4acaee4997e41b1a527d9e1
Daniel Gogov <danielgogovmail@gmail.com>
2021-03-08 20:51:16 +0100

    Circle in circle

--- index.js
+++ index.js
@@ -3,3 +3,15 @@
 // pdf.addHTML(document.querySelectorAll('.wrapper'), function () {
 //     pdf.save('Test.pdf');
 // });
+
+let buttonRotate = document.querySelector('.rotate');
+let circle = document.querySelector('.circle');
+let circleInfo = document.querySelector('.circle-info');
+let rotate = 0;
+let rotateStep = 40;
+
+buttonRotate.addEventListener('click', function() {
+    rotate += rotateStep;
+    circle.style.transform = 'rotate('+ rotate +'deg)';
+    circleInfo.style.transform = 'rotate(-'+ rotate +'deg)'
+});
