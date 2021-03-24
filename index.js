f032c50b82e0b1b224ceda64dd47545f3f3fc451
Daniel Gogov <danielgogovmail@gmail.com>
2021-03-24 22:31:43 +0100

    Added JS Decimal and Updated

--- index.js
+++ index.js
@@ -4,14 +4,20 @@
 //     pdf.save('Test.pdf');
 // });
 
-let buttonRotate = document.querySelector('.rotate');
-let circle = document.querySelector('.circle');
-let circleInfo = document.querySelector('.circle-info');
-let rotate = 0;
-let rotateStep = 40;
+// let buttonRotate = document.querySelector('.rotate');
+// let circle = document.querySelector('.circle');
+// let circleInfo = document.querySelector('.circle-info');
+// let rotate = 0;
+// let rotateStep = 40;
 
-buttonRotate.addEventListener('click', function() {
-    rotate += rotateStep;
-    circle.style.transform = 'rotate('+ rotate +'deg)';
-    circleInfo.style.transform = 'rotate(-'+ rotate +'deg)'
-});
+// buttonRotate.addEventListener('click', function() {
+//     rotate += rotateStep;
+//     circle.style.transform = 'rotate('+ rotate +'deg)';
+//     circleInfo.style.transform = 'rotate(-'+ rotate +'deg)'
+// });
+
+function insertDecimal(num) {
+    return (num / 1000).toFixed(3);
+}
+
+console.log(insertDecimal(1000));
